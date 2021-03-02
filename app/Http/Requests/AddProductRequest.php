a0c994b824b9692105eebe417cfb067688c8e067
= <=>
2021-03-02 11:57:40 +0700

    sua validate product

--- app/Http/Requests/AddProductRequest.php
+++ app/Http/Requests/AddProductRequest.php
@@ -31,8 +31,8 @@ class AddProductRequest extends FormRequest
          'price' =>'required|digits_between:4,11',
          'discount' =>'required|digits_between:4,11',
          'quality' =>'required|digits_between:4,11',
-         'image' =>'required|image|max:10000',
-         'avatar' =>'required|image|max:10000',
+         'image' =>'required|max:10000',
+         'avatar' =>'required|max:10000',
         ];
     }
 
@@ -43,7 +43,6 @@ class AddProductRequest extends FormRequest
             'unique'=>':attribute đã được sử dụng',
             'digits_between'=>':attribute phải là số và từ 4 đến 11 số',
             'max' => ':attribute kích thước không được vượt quá 10000kb',
-            'image' => ':attribute phải là ảnh'
         ];
     }
 
