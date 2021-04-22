430e9aaac3ddd92c2f884ece9d1415be36af24d0
NinhFPT14 <77847054+NinhFPT14@users.noreply.github.com>
2021-04-22 13:26:17 +0700

    xoa bang product vouchers

--- database/migrations/2021_04_22_132512_delete_product_voucher.php
+++ database/migrations/2021_04_22_132512_delete_product_voucher.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddNamePriceProductOrder extends Migration
+class DeleteProductVoucher extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,7 @@ class AddNamePriceProductOrder extends Migration
      */
     public function up()
     {
-        Schema::table('product_oders', function (Blueprint $table) {
-            $table->string('name');
-            $table->integer('price');
-        });
+        Schema::dropIfExists('product_vouchers');
     }
 
     /**
@@ -26,7 +23,7 @@ class AddNamePriceProductOrder extends Migration
      */
     public function down()
     {
-        Schema::table('product_oders', function (Blueprint $table) {
+        Schema::table('product_vouchers', function (Blueprint $table) {
             //
         });
     }
