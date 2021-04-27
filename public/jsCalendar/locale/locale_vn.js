34f8fb74f5035baa9fb7f2281e8ced46cf67493e
HaiYeuDoLa <46549236+lucloithanki@users.noreply.github.com>
2021-04-27 17:56:30 +0700

    upate-data-to-table

---
+++ public/jsCalendar/locale/locale_vn.js
@@ -0,0 +1,80 @@
+/*
+
+@license
+dhtmlxScheduler v.5.3.11 Professional Evaluation
+
+This software is covered by DHTMLX Evaluation License. Contact sales@dhtmlx.com to get Commercial or Enterprise license. Usage without proper license is prohibited.
+
+(c) XB Software Ltd.
+
+*/
+Scheduler.plugin(function(e) {
+    e.locale = {
+        date: { month_full: ["Tháng 1", "Tháng 2", "Tháng 3", "Tháng 4", "Tháng 5", "Tháng 6", "Tháng 7", "Tháng 8 ", " Tháng 9 ", " Tháng 10 ", " Tháng 11 ", " Tháng 12 "], month_short: [" Tháng 1 ", " Tháng 2 ", " Tháng 3 ", " Tháng 4 ", " Tháng 5 ", " Tháng 6 ", " Tháng 7 ", "Tháng 8", "Tháng 9", "Tháng 10", "Tháng 11", "Tháng 12"], day_full: ["Chủ nhật", "Thứ Hai", "Thứ Ba", "Thứ Tư", "Thứ Năm", "Thứ Sáu", "Thứ Bảy "], day_short: [" CN ", " Mon ", " Tue ", " Wed ", " Thu ", " Fri ", " Sat "] },
+        labels: {
+            dhx_cal_today_button: "Hôm Nay",
+            day_tab: "Ngày",
+            week_tab: "Tuần",
+            month_tab: "Tháng",
+            new_event: "Sự kiện mới",
+            icon_save: "Lưu",
+            icon_cancel: "Hủy",
+            icon_details: "Chi tiết",
+            icon_edit: "Chỉnh sửa",
+            icon_delete: "Xóa",
+            confirm_closing: "",
+            confirm_deleting: "Sự kiện sẽ bị xóa vĩnh viễn, bạn có chắc không?",
+            section_description: "Description",
+            section_time: "Khoảng thời gian",
+            full_day: "Cả ngày",
+            confirm_recurring: "Bạn có muốn chỉnh sửa toàn bộ tập hợp các sự kiện lặp lại không?",
+            section_recurring: "Lặp lại sự kiện",
+            button_recurring: "Đã tắt",
+            button_recurring_open: "Đã bật",
+            button_edit_series: "Chỉnh sửa chuỗi",
+            button_edit_occurrence: "Chỉnh sửa lần xuất hiện",
+            agenda_tab: "Agenda",
+            date: "Ngày",
+            description: "Description",
+            year_tab: "Năm",
+            week_agenda_tab: "Agenda",
+            grid_tab: "Grid",
+            drag_to_create: "Kéo để tạo",
+            drag_to_move: "Kéo để di chuyển",
+            message_ok: "OK",
+            message_cancel: "Hủy",
+            next: "Tiếp",
+            prev: "Quay Lại",
+            year: "Năm",
+            month: "Tháng",
+            day: "Ngày",
+            hour: "Giờ",
+            minute: "Phút",
+            repeat_radio_day: "Hàng ngày",
+            repeat_radio_week: "Hàng tuần",
+            repeat_radio_month: "Hàng tháng",
+            repeat_radio_year: "Hàng năm",
+            repeat_radio_day_type: "Mọi",
+            repeat_text_day_count: "day",
+            repeat_radio_day_type2: "Mỗi ngày làm việc",
+            repeat_week: "Lặp lại mỗi ngày",
+            repeat_text_week_count: "tuần tới:",
+            repeat_radio_month_type: "Lặp lại",
+            repeat_radio_month_start: "Bật",
+            repeat_text_month_day: "On",
+            repeat_text_month_count: "місяць",
+            repeat_text_month_count2_before: "кожен ",
+            repeat_text_month_count2_after: "місяць",
+            repeat_year_label: "",
+            select_year_day2: "",
+            repeat_text_year_day: "day",
+            select_year_month: "",
+            repeat_radio_end: "end",
+            repeat_text_occurences_count: "lần xuất hiện",
+            repeat_radio_end3: "Kết thúc bởi",
+            repeat_radio_end2: "",
+            month_for_recurring: ["Tháng 1", "Tháng 2", "Tháng 3", "Tháng 4", "Tháng 5", "Tháng 6", "Tháng 7", "Tháng 8", "Tháng 9", "Tháng 10", "Tháng 11", "Tháng 12"],
+            day_for_recurring: ["Chủ nhật", "Thứ Hai", "Thứ Ba", "Thứ Tư", "Thứ Năm", "Thứ Sáu", "Thứ Bảy "]
+        }
+    }
+});
--- resources/views/backend/services/sortAppointment.blade.php
+++ resources/views/backend/services/sortAppointment.blade.php
@@ -9,13 +9,14 @@ Bảng xếp lịch
 <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 <link rel='stylesheet' href='https://cdn.rawgit.com/t4t5/sweetalert/v0.2.0/lib/sweet-alert.css'>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/css/select2.min.css">
-<script src="{{ asset('jsCalendar/dhtmlxscheduler.js') }}" type="text/javascript" charset="utf-188"></script>
+<script src="{{ asset('jsCalendar/dhtmlxscheduler.js') }}" type="text/javascript" charset="utf-8"></script>
     <script src=" {{ asset('jsCalendar/ext/dhtmlxscheduler_timeline.js') }} " type="text/javascript" charset="utf-8"></script>
-
+    <script src=" {{ asset('jsCalendar/locale/locale_vn.js') }} " type="text/javascript" charset="utf-8"></script>
     <link rel='stylesheet' type='text/css' href="{{ asset('jsCalendar/dhtmlxscheduler_material.css') }}">
 @endsection
-<div class="md:container md:mx-auto px-4 border-green-900 h-5/6  shadow-xl ">
-       <div id="scheduler_here" class="dhx_cal_container" style='width:100%; height:100%'>
+<div class="grid grid-cols-3">
+<div class="md:container px-12 border-green-900 h-screen col-span-2 shadow-xl ">
+       <div id="scheduler_here" class="dhx_cal_container px-6" style='width:100%; height:100%'>
                    <div class="dhx_cal_navline">
                        <div class="dhx_cal_prev_button">&nbsp;</div>
                        <div class="dhx_cal_next_button">&nbsp;</div>
@@ -28,6 +29,11 @@ Bảng xếp lịch
        </div>
        <!-- Bảng xếp lịch kết thúc -->
 </div>
+
+<div class="col-span-1 ">
+</div>
+</div>
+
 <br>
 
 
@@ -37,17 +43,16 @@ Bảng xếp lịch
 <script type="text/javascript" charset="utf-8">
 	var sections = {!!json_encode($seats)!!};
 	var data = {!!json_encode($list)!!};
-	console.log(data)
+	
 	window.addEventListener("DOMContentLoaded", function(){
-		
 			scheduler.locale.labels.timeline_tab = "Timeline";
 			scheduler.locale.labels.section_custom = "Section";
 			scheduler.config.multisection = true;
-            // scheduler.config.readonly = true;
+            scheduler.config.readonly = true;
 			//===============
 			//Configuration
 			//===============
-
+			//===============================
 			var durations = {
 				day: 24 * 60 * 60 * 1000,
 				hour: 60 * 60 * 1000,
@@ -95,32 +100,11 @@ Bảng xếp lịch
 
 			});
 
-			scheduler.attachEvent("onEventSave",function(id,ev,is_new){
-				var convert = scheduler.date.date_to_str("%Y/%m/%d %H:%i", true); 
-				const url = "{{ route('appointment.apiSave') }}"
-				console.log(ev.text)
-				console.log(convert(ev.start_date))
-				console.log(convert(ev.end_date))
-				if (!ev.text) {
-					alert("Text must not be empty");
-					return false;
-				}
-				if (!ev.text.length<20) {
-					alert("Text too small");
-					return false;
-				}
-				return true;
-				axios.post(url, {
-
-				})
-				
-			})
-
 
 			//===============
 			//Data loading
 			//===============
-            var dateToStr = scheduler.date.date_to_str("%Y-%m-%d");
+            var dateToStr = scheduler.date.date_to_str("%d-%m-%Y");
 
             scheduler.templates.format_date = function(date){
                 return dateToStr (date);
